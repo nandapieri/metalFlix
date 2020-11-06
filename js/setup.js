ab8c6ebe29fcf678b6c42b5eee129e444e623957
Fernanda Pieri <nandapieri@gmail.com>
2020-11-06 02:12:30 +0000

    estilização dos carrosseis

--- js/setup.js
+++ js/setup.js
@@ -1,19 +1,16 @@
-    $('.owl-carousel').owlCarousel({
-        items:1,
-        merge:true,
-        loop:true,
-        margin:10,
-        video:true,
-        lazyLoad:false,
-        center:true,
-        videoWidth: true,
-        videoHeight: true,
-        responsive:{
-            480:{
-                items:2
-            },
-            600:{
-                items:4
-            }
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:false,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
         }
-    })
+    }
+})
